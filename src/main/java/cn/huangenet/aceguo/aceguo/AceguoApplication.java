9a2dd37673d868c28fc73aa14d6e835c051f5420
AceGuoX <1146913546@qq.com>
2021-06-28 17:01:00 +0800

    fist commit

---
+++ src/main/java/cn/huangenet/aceguo/aceguo/AceguoApplication.java
@@ -0,0 +1,13 @@
+package cn.huangenet.aceguo.aceguo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceguoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceguoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/huangenet/aceguo/aceguo/AceguoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.huangenet.aceguo.aceguo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceguoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
